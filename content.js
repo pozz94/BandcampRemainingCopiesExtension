6c54045bfb385557c0c0ed048544a48839b8f0fa
pozz94 <davidepzzn@gmail.com>
2018-07-09 02:17:02 +0200

    Add files via upload

--- content.js
+++ content.js
@@ -1,8 +1,19 @@
-console.log('bandcamp remaining copies extension executing');
+window.addEventListener("load", function(){
+    //since bandcamp allows for custom url the extension needs to run on all pages so to filter out pages that are not bandcamp I do:
+    if(document.getElementById("login")!=null && typeof document.getElementById("login").childNodes[0].getAttribute('href') !== "undefined")
+    {
+        let login=document.getElementById("login").childNodes[0].getAttribute('href');
+        if (login.indexOf("bandcamp.com"))
+        {
+            console.log("this is Bandcamp!");
+            console.log('bandcamp remaining copies extension executing');
 
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('script.js');
-s.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(s);
+            var s = document.createElement('script');
+            s.src = chrome.extension.getURL('script.js');
+            s.onload = function() {
+                this.remove();
+            };
+            (document.head || document.documentElement).appendChild(s);
+        }
+    }
+});
--- script.js
+++ script.js
@@ -15,28 +15,31 @@ function waitForVar(variable, callback)
 function extension()
 {
     waitForVar("TralbumData", function(){
-        var content = '';
-        var quantity = document.createElement('div');
+        let content = '';
+        let quantity = document.createElement('div');
 
-        TralbumData['packages'].forEach(element => {
-            console.log(element['title'] + ': ' + element['quantity_available'] + ' copies still available  ');
-            if (element['quantity_available'] == 1)
-                content += '<h3 class="tags-inline-label">' + element['title'] + ': ' + element['quantity_available'] + ' copy still available</h3>';
-            else if(element['quantity_available'] == null)
-                content += '<h3 class="tags-inline-label">' + element['title'] + ': infinite copies could still be available as far as I know (couldn\'t read the number ;b)</h3>';
-            else
-                content += '<h3 class="tags-inline-label">' + element['title'] + ': ' + element['quantity_available'] + ' copies still available</h3>';
-        });
+        if(TralbumData['packages'] != null && typeof TralbumData['packages'] !== "undefined")
+        {
+            TralbumData['packages'].forEach(element => {
+                console.log(element['title'] + ': ' + element['quantity_available'] + ' copies still available  ');
+                if (element['quantity_available'] == 1)
+                    content += '<h3 class="tags-inline-label">' + element['title'] + ': ' + element['quantity_available'] + ' copy still available</h3>';
+                else if(element['quantity_available'] == null)
+                    content += '<h3 class="tags-inline-label">' + element['title'] + ': infinite copies could still be available as far as I know (couldn\'t read the number ;b)</h3>';
+                else
+                    content += '<h3 class="tags-inline-label">' + element['title'] + ': ' + element['quantity_available'] + ' copies still available</h3>';
+            });
 
-        quantity.innerHTML= '<h3 class="tags-inline-label">' + content + '</h3>';
-        quantity.className = "remainingQuantity";
-        quantity.style.width = "100vw";
-        quantity.style.width = "auto";
-        //quantity.style.position = "fixed";
-        quantity.onload = function() {
-            this.remove();
-        };
-        document.getElementById("name-section").appendChild(quantity);
+            quantity.innerHTML= '<h3 class="tags-inline-label">' + content + '</h3>';
+            quantity.className = "remainingQuantity";
+            quantity.style.width = "100vw";
+            quantity.style.width = "auto";
+            //quantity.style.position = "fixed";
+            quantity.onload = function() {
+                this.remove();
+            };
+            document.getElementById("name-section").appendChild(quantity);
+        }
     });
 }
 
