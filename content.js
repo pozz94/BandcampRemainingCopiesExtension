4684bfe7d7c8d0eebd1973c3e56dec2ced987d9d
pozz94 <davidepzzn@gmail.com>
2018-07-09 09:25:35 +0200

    Add files via upload

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", function(){
     //since bandcamp allows for custom url the extension needs to run on all pages so to filter out pages that are not bandcamp I do:
-    if(document.getElementById("login")!=null && typeof document.getElementById("login").childNodes[0].getAttribute('href') !== "undefined")
+    if(document.getElementById("footer-logo")!=null && typeof document.getElementById("footer-logo").getAttribute('href') !== "undefined")
     {
-        let login=document.getElementById("login").childNodes[0].getAttribute('href');
-        if (login.indexOf("bandcamp.com"))
+        let logo=document.getElementById("footer-logo").getAttribute('href');
+        if (logo.indexOf("bandcamp.com"))
         {
             console.log("this is Bandcamp!");
             console.log('bandcamp remaining copies extension executing');
