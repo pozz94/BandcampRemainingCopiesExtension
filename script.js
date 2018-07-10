78555a5e9e709dfd3dc4b380bae9bd8917d609f3
pozz94 <davidepzzn@gmail.com>
2018-07-10 18:23:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,108 @@
-function waitForVar(variable, callback)
-{
-    if (window[variable]!==undefined)
-    {
-        callback();
-    }
-    else
-    {
-        setTimeout(function() {
-            waitForVar(variable, callback);
-        }, 100);
-    }
-}
+let update = new Event('inventoryUpdate');
 
 function extension()
 {
-    waitForVar("TralbumData", function(){
-        let content = '';
-        let quantity = document.createElement('div');
+	let content = {item: [], value:''};
+	let quantity = document.createElement('div');
 
-        if(TralbumData['packages'] != null && typeof TralbumData['packages'] !== "undefined")
-        {
-            TralbumData['packages'].forEach(element => {
-                console.log(element['title'] + ': ' + element['quantity_available'] + ' copies still available  ');
-                if (element['quantity_available'] == 1)
-                    content += '<h3 class="tags-inline-label">' + element['title'] + ': ' + element['quantity_available'] + ' copy still available</h3>';
-                else if(element['quantity_available'] == null)
-                    content += '<h3 class="tags-inline-label">' + element['title'] + ': infinite copies could still be available as far as I know (couldn\'t read the number ;b)</h3>';
-                else
-                    content += '<h3 class="tags-inline-label">' + element['title'] + ': ' + element['quantity_available'] + ' copies still available</h3>';
-            });
+	quantity.className = 'remainingQuantity buyItem';
+	quantity.style.width = "auto";
 
-            quantity.innerHTML= '<h3 class="tags-inline-label">' + content + '</h3>';
-            quantity.className = "remainingQuantity";
-            quantity.style.width = "100vw";
-            quantity.style.width = "auto";
-            //quantity.style.position = "fixed";
-            quantity.onload = function() {
-                this.remove();
-            };
-            document.getElementById("name-section").appendChild(quantity);
-        }
-    });
+	waitForVar('TralbumData', () => {
+		if(TralbumData.packages != null && typeof TralbumData.packages !== "undefined")
+		{
+			window.addEventListener('inventoryUpdate', debounceEvent(() => {
+				console.log('Inventory update, building new HTML for items in stock');
+				buildHTML(TralbumData.packages, content, quantity);
+			}, 100));
+			window.dispatchEvent(update);
+		}
+	});
+}
+
+function buildHTML(parent, html, elmt)
+{
+	let index = 0;
+	parent.forEach(element => {
+		//watch variable to rebuild HTML in case it 
+		watchVariabile(element, 'quantity_available', () => {
+			window.dispatchEvent(update);
+			//buildHTML(parent, html, elmt);
+		});
+
+		let number=element.quantity_available;
+
+		if (number == 0)
+			html.item[index] = '<span style="font-style: italic; font-weight:normal">no copies available</span>';
+		else if(number == null)
+			html.item[index] = '<span style="font-style: italic; font-weight:normal">no availability info or &infin;</span>';
+			else if (number == 1)
+			html.item[index] = '<span class="notable">1</span><span style="font-weight:normal"> copy in stock</span>';
+		else
+			html.item[index] = '<span class="notable">' + number + '</span><span style="font-weight:normal"> copies in stock</span>';
+
+		html.item[index] = '<span class="lightweightBreak"></span><h3 class="tags-inline-label">' + element.title + ': '+ html.item[index] + '</h3>'
+		
+		index++;
+		if(index === TralbumData.packages.length)
+		{
+			appendToTitle(elmt, html);
+		}
+	});
+}
+
+function appendToTitle(toAppend, HTML)
+{
+	HTML.item.forEach(element => HTML.value += element);
+	toAppend.innerHTML = HTML.value;
+	HTML.item = [];
+	HTML.value = '';
+	toAppend.onload = function() {
+		this.remove();
+	};
+	document.getElementById("name-section").appendChild(toAppend);
 }
 
 function insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
+
+function watchVariabile (object, name, callback)
+{
+	let value = object[name];
+
+	Object.defineProperty(object, name, {
+
+		get: () => { 
+			return value; 
+		},
+
+		set: (newValue) => { 
+			value = newValue; 
+			callback (value);
+		},
+
+		enumerable: true,
+		configurable: true
+	});
+}
+
+function waitForVar(variable, callback)
+{
+	if (window[variable] !== undefined)
+	{
+		callback();
+	}
+	else
+	{
+		setTimeout(() => {
+			waitForVar(variable, callback);
+		}, 100);
+	}
+}
+
+const debounceEvent = (callback, time = 250, interval) => 
+	(...args) =>
+		clearTimeout(interval, interval = setTimeout(() => callback(...args), time));
 
 window.addEventListener('load', extension());
